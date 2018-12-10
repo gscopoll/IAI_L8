8d4ae8cb9c6496c4cf6538aef90c169185a6644a
Kamil Krogulski <szarik514@gmail.com>
2018-12-10 18:14:58 +0100

    cwiczenia_9

--- block/src/app/app.module.ts
+++ block/src/app/app.module.ts
@@ -1,9 +1,31 @@
+import {RouterModule, Routes} from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+const appRoutes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'blog',
+    component: BlogComponent
+  },
+  {
+    path: 'map',
+    component: MapComponent
+  },
+  {
+    path: 'quiz',
+    component: QuizComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  }
+  ];
 
 @NgModule({
   declarations: [
